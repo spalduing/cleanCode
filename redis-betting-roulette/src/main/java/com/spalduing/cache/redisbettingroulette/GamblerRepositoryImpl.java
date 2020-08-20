9e502f0ad90723fa602aee2e63e0c89e791bf721
Miguelangel Diaz <spalduing12@gmail.com>
2020-08-20 17:20:14 -0500

    first RouletteApp commit

---
+++ redis-betting-roulette/src/main/java/com/spalduing/cache/redisbettingroulette/GamblerRepositoryImpl.java
@@ -0,0 +1,46 @@
+package com.spalduing.cache.redisbettingroulette;
+
+import com.spalduing.cache.redisbettingroulette.gambler.Gambler;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public class GamblerRepositoryImpl implements GamblerRepository {
+    private RedisTemplate<String, Gambler> redisTemplate;
+    private HashOperations hashOperations;
+    public GamblerRepositoryImpl(RedisTemplate<String, Gambler> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        hashOperations = redisTemplate.opsForHash();
+    }
+
+
+    @Override
+    public void save(Gambler gambler) {
+        hashOperations.put("GAMBLER", gambler.getId(), gambler);
+    }
+
+    @Override
+    public Map<String, Gambler> findAll() {
+        return hashOperations.entries("GAMBLER");
+    }
+
+    @Override
+    public Gambler findById(Gambler gambler) {
+        String id = gambler.getId();
+        return (Gambler)hashOperations.get("GAMBLER", id);
+    }
+
+    @Override
+    public void update(Gambler gambler) {
+        save(gambler);
+    }
+
+    @Override
+    public void delete(Gambler gambler) {
+        String id = gambler.getId();
+        hashOperations.delete("GAMBLER", id);
+    }
+}
