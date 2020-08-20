9f35770468dd125ea5524740ecb37bb74fbd239d
Miguelangel Diaz <spalduing12@gmail.com>
2020-08-20 18:24:25 -0500

    missin files added

---
+++ redis-betting-roulette/src/test/java/com/spalduing/cache/redisbettingroulette/RedisBettingRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spalduing.cache.redisbettingroulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisBettingRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
