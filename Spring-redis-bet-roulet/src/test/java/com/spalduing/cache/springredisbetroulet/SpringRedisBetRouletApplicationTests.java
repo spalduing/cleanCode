51f9b5ef2e26d54436f1e6d17601cd95be493557
Miguelangel Diaz <spalduing12@gmail.com>
2020-08-23 09:59:22 -0500

    Jedis and Apache-common-pools2 dependencies added

---
+++ Spring-redis-bet-roulet/src/test/java/com/spalduing/cache/springredisbetroulet/SpringRedisBetRouletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spalduing.cache.springredisbetroulet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisBetRouletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
